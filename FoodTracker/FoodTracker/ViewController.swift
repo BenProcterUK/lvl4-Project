714b9bb1c7b66913364219d7c42349de163cc367
Ben Procter <benprocter@hotmail.co.uk>
2016-11-29 09:26:54 +0000

    Uploaded test project for learning Swift

---
+++ FoodTracker/FoodTracker/ViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  VoteTracker
+//
+//  Created by Ben Procter on 08/11/2016.
+//  Copyright © 2016 Ben Procter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITextFieldDelegate {
+        //MARK: Properties
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var mealNameLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //Handle the text field's user input through delegate callbacks.
+        nameTextField.delegate = self
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    //MARK: UITextFieldDelegate
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+    // Hide the keyboard.
+    textField.resignFirstResponder()
+    return true
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        mealNameLabel.text = textField.text 
+    }
+    
+    //MARK: Actions
+    @IBAction func setDefaultLabelText(_ sender: UIButton) {
+        mealNameLabel.text = ("Welcome to VoteTracker " + nameTextField.text!)
+    }
+    
+}
+
---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,13 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+let number = 23
+
+if number > 100 {
+    print( "This number is pretty big")
+}
+else {
+    print (str) }
+
